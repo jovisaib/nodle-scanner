de5eb3cfb0eef1a30efede6c5d7b5270e2e8cc72
jovisaib <jovisaib@gmail.com>
2022-09-30 13:00:16 +0200

    hotfix at addresses

--- scripts.sh
+++ scripts.sh
@@ -1,13 +1,8 @@
-npm run to_pubsub --topic=test-nodle-scanner --startblock=304864 --endblock=404864
+npm run to_pubsub --dataset=ble_event --table=nodl_transfers_2022sept --startblock=304864 --endblock=404864
+npm run to_pubsub --dataset=ble_event --table=nodl_transfers_2022sept --startblock=404865 --endblock=504865
+npm run to_pubsub --dataset=ble_event --table=nodl_transfers_2022sept --startblock=504866 --endblock=604866
+npm run to_pubsub --dataset=ble_event --table=nodl_transfers_2022sept --startblock=604867 --endblock=704867
+npm run to_pubsub --dataset=ble_event --table=nodl_transfers_2022sept --startblock=704868 --endblock=804868
+npm run to_pubsub --dataset=ble_event --table=nodl_transfers_2022sept --startblock=804869 --endblock=904869
+npm run to_pubsub --dataset=ble_event --table=nodl_transfers_2022sept --startblock=904870 --endblock=1004870
 
-npm run to_pubsub --topic=test-nodle-scanner --startblock=404865 --endblock=504865
-
-npm run to_pubsub --topic=test-nodle-scanner --startblock=504866 --endblock=604866
-
-npm run to_pubsub --topic=test-nodle-scanner --startblock=604867 --endblock=704867
-
-npm run to_pubsub --topic=test-nodle-scanner --startblock=704868 --endblock=804868
-
-npm run to_pubsub --topic=test-nodle-scanner --startblock=804869 --endblock=904869
-
-npm run to_pubsub --topic=test-nodle-scanner --startblock=904870 --endblock=1004870
