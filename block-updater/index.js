93d21c06bb0aab318ec7c186de9bf3272f9160db
jovisaib <jovisaib@gmail.com>
2022-10-10 12:01:34 +0200

    added account updater

---
+++ block-updater/index.js
@@ -0,0 +1,74 @@
+import { ApiPromise, WsProvider } from '@polkadot/api'
+import { encodeAddress } from '@polkadot/util-crypto'
+import { stringify } from 'csv-stringify';
+import * as fs from 'fs';
+
+const nodleTestNode = 'wss://nodle-paradis.api.onfinality.io/public-ws'
+const nodleProdNode = 'wss://nodle-parachain.api.onfinality.io/public-ws'
+const nodleNode = nodleProdNode;
+
+async function main() {
+    // Substrate node we are connected to and listening to remarks
+    const provider = new WsProvider(nodleNode);
+    const api = await ApiPromise.create({ provider });
+
+    // Get general information about the node we are connected to
+    const [chain, nodeName, nodeVersion] = await Promise.all([
+        api.rpc.system.chain(),
+        api.rpc.system.name(),
+        api.rpc.system.version()
+    ]);
+    console.log(
+        `You are connected to chain ${chain} using ${nodeName} v${nodeVersion}`
+    );
+
+
+    let stream = stringify({
+        header: true, columns: [
+            "account_id",
+            "free_balance",
+            "reserved_balance",
+        ]
+    });
+
+    stream.pipe(fs.createWriteStream("./accounts.csv"))
+    const properties = await api.rpc.system.properties()
+    const decimalst = properties.tokenDecimals.unwrapOr([12])[0]
+    const decimals = parseInt(decimalst.toString());
+
+    // Adjust how many accounts to query at once.
+    let limit = 1000;
+    let total = 0;
+    //let result = [];
+    let last_key = ""
+
+    const start = Date.now()
+
+    while (true) {
+        let query = await api.query.system.account.entriesPaged({ args: [], pageSize: limit, startKey: last_key });
+
+        if (query.length == 0) {
+            break
+        }
+        let batch = 0
+        for (const user of query) {
+            let account_id = encodeAddress(user[0].slice(-32), 37);
+            let free_balance = user[1].data.free.toString();
+            let reserved_balance = user[1].data.reserved.toString();
+            //result.push();
+            stream.write({ account_id: account_id, free_balance: free_balance / Math.pow(10, decimals), reserved_balance: reserved_balance / Math.pow(10, decimals) })
+            last_key = user[0];
+            batch++
+        }
+        total += batch
+        console.log("fetched " + batch + ", total " + total);
+    }
+    const stop = Date.now()
+    console.log(`Time Taken to execute = ${(stop - start) / 1000} seconds`);
+}
+
+
+main().catch((error) => {
+    console.error(error);
+    process.exit(-1);
+});
--- scripts.sh
+++ scripts.sh
@@ -4,6 +4,7 @@ DONE: npm run to_pubsub --dataset=ble_event --table=nodl_transfers_2022sept --st
 DONE: npm run to_pubsub --dataset=ble_event --table=nodl_transfers_2022sept --startblock=604867 --endblock=704867
 DONE: npm run to_pubsub --dataset=ble_event --table=nodl_transfers_2022sept --startblock=704868 --endblock=804868
 DONE: npm run to_pubsub --dataset=ble_event --table=nodl_transfers_2022sept --startblock=804869 --endblock=904869
+DONE: npm run to_pubsub --dataset=ble_event --table=nodl_transfers_2022sept --startblock=904870 --endblock=1004870
+DONE: npm run to_pubsub --dataset=ble_event --table=nodl_transfers_2022sept --startblock=1004871 --endblock=1104871
 
-npm run to_pubsub --dataset=ble_event --table=nodl_transfers_2022sept --startblock=904870 --endblock=1004870
-npm run to_pubsub --dataset=ble_event --table=nodl_transfers_2022sept --startblock=1004871 --endblock=1104871
+-: npm run to_pubsub --dataset=ble_event --table=nodl_transfers_2022sept --startblock=1104872 --endblock=1133448
